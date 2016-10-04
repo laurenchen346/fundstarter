75064e9cdeaca3c2b0cd3aae260ba9e7d5151953
Ubuntu <ubuntu@ip-172-31-41-39.us-west-2.compute.internal>
2016-10-04 02:42:28 +0000

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('public/index.html',{root:__dirname});
+var http=require("http");
+var fs=require("fs");
+var fileName="public/index.html";
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
+
+http.createServer(function(request,response) {
+  
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+            var buffer=new Buffer(stats.size);
+            fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+        if (error) {
+            response.writeHeader(404, {"Content-Type":"html"});
+            response.write("404 Not Found\n");
+            response.end();
+        } else {
+            response.writeHeader(200);
+            response.write(buffer);
+            response.end();
+        }
+    });
+
+          fs.close(fd);
+    
+      });
+    });
+  }
+});
 }).listen(8080);
 
 console.log("Server Running on 8080");
